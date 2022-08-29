75113ba226b387286915fb331b6ce106844c91f3
bunny <deamonbunny@gmail.com>
2022-08-28 18:11:59 -0700

    filled our api routes files

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+    createThought,
+    getThoughts,
+    getSingleThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController')
+
+router.route('/').get(getThoughts).post(createThought);
+
+router 
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought)
+
+router.route('/:thoughtId/reaction').post(addReaction);
+
+router.route('/:thoughtId/reactions:reactionId').delete(deleteReaction)
+
+module.exports = router;
