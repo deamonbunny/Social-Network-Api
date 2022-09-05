48c73fa2e1c8b6670dfde004f171a5f7147b8a8f
bunny <deamonbunny@gmail.com>
2022-09-04 19:33:01 -0700

    swapped app.use statements to get intented outcomes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const routes = require("./routes");
 const app = express();
 const PORT = 3001;
 
-app.use(routes);
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(routes);
 
-app.get("*", (req,res) => res.status(404).json({message:"This doesn't seem to exist."}));
+// app.get("*", (req,res) => res.status(404).json({message:"This doesn't seem to exist."}));
 db.once('open', () =>{
     app.listen(PORT, () => {
         console.log(`Your API is now running on server port ${PORT}`);
